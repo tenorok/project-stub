4e36d3585d2f4eaca8b5e82b5f8e71188354bdb4
tenorok <ttenor@yandex.ru>
2013-10-04 15:41:11 +0400

    Одна карточка

---
+++ desktop.blocks/contact/__photo/contact__photo.deps.js
@@ -0,0 +1,5 @@
+({
+    shouldDeps: [
+        { elem: 'photo-wrap' }
+    ]
+})
--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,29 +1,103 @@
 ({
     block: 'page',
-    title: 'Title of the page',
+    title: 'Контакты',
     favicon: '/favicon.ico',
     head: [
-        { elem: 'css', url: '_index.css', ie: false },
-        { elem: 'css', url: '_index', ie: true },
-        { elem: 'meta', attrs: { name: 'description', content: '' }}
+        { elem: 'css', url: '_index.css', ie: false }
     ],
     content:[
         {
-            block: 'header',
+            block: 'contact',
+            mix: { block: 'clearfix' },
             content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
+                {
+                    elem: 'photo',
+                    content: 'http://jing.yandex-team.ru/files/tenorok/screen01_2013-10-04_15-36-22.png'
+                },
+                {
+                    elem: 'group',
+                    content: 'семья'
+                },
+                {
+                    elem: 'info',
+                    content: [
+                        {
+                            elem: 'name',
+                            content: 'Гомер Симпсон'
+                        },
+                        {
+                            elem: 'phone',
+                            content: {
+                                block: 'button',
+                                mix: { block: 'contact', elem: 'phone-button' },
+                                content: '+7 987 456-71-28'
+                            }
+                        },
+                        {
+                            elem: 'address',
+                            content: 'Спрингфилд, 36'
+                        },
+                        {
+                            elem: 'email',
+                            content: {
+                                block: 'link',
+                                url: 'mailto:homer@yandex.ru',
+                                content: 'homer@yandex.ru'
+                            }
+                        },
+                        {
+                            elem: 'more',
+                            content: [
+                                {
+                                    elem: 'details',
+                                    content: [
+                                        {
+                                            elem: 'detail',
+                                            field: 'День рождения',
+                                            value: '1 января 1970'
+                                        },
+                                        {
+                                            elem: 'detail',
+                                            field: 'Место работы',
+                                            value: 'АЭС'
+                                        },
+                                        {
+                                            elem: 'detail',
+                                            field: 'Отдел',
+                                            value: 'Департамент утечек атомного реагента'
+                                        },
+                                        {
+                                            elem: 'detail',
+                                            field: 'Должность',
+                                            value: 'Специалист по безопасности'
+                                        }
+                                    ]
+                                },
+                                {
+                                    elem: 'links',
+                                    content: [
+                                        {
+                                            elem: 'social',
+                                            mods: { service: 'facebook' },
+                                            content: 'facebook',
+                                            url: 'https://www.facebook.com/pages/Homer-Simpson/109386305754801'
+                                        },
+                                        {
+                                            elem: 'social',
+                                            mods: { service: 'vkontakte' },
+                                            content: 'vkontakte'
+                                        },
+                                        {
+                                            elem: 'social',
+                                            mods: { service: 'twitter' },
+                                            content: 'twitter'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
             ]
         },
         { elem: 'js', url: '_index.js' }
